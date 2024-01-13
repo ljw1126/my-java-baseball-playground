542ed5a4b0e2113ab8ff9b039ef15b74f5b2e275
이진우 <leejinwoo1126@gmail.com>
2024-01-13 21:58:41 +0900

    test: Balls @ParameterizedTest 추가

--- src/test/java/BallsTest.java
+++ src/test/java/BallsTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.util.Arrays;
 
@@ -38,5 +39,17 @@ class BallsTest {
     void nothing() {
         PlayResult result = balls.play(Arrays.asList(4, 5, 6));
         assertThat(result.getResult()).isEqualTo("낫싱");
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,2,3,3 스트라이크 ", "1,3,2, 1 스트라이크 2 볼 ", "3,1,2,3 볼", "4,5,6,낫싱"})
+    void parameterTest(int ball1, int ball2, int ball3, String expected) {
+        // given
+        // when
+        PlayResult result = balls.play(Arrays.asList(ball1, ball2, ball3));
+        String actual = result.getResult().trim();
+
+        // then
+        assertThat(actual).isEqualTo(expected);
     }
 }
